be0ef65b2b86a600ebcf3a0b7e173a64c946609f
DustyPolk <119711371+DustyPolk@users.noreply.github.com>
2024-02-06 13:11:13 -0500

    Add files via upload

---
+++ win_du.ps1
@@ -0,0 +1,15 @@
+﻿function du {
+
+$FinalResult = @()
+Get-ChildItem -Directory -Force | foreach-object {
+$totalsize = [long]0
+Get-ChildItem -File -Recurse -Force -ErrorAction SilentlyContinue -Path $_.Name | ForEach-Object {$totalsize += $_.Length}
+
+$FinalResult += New-Object psobject -Property @{
+                        Dir = $_.Name
+                        Size = $totalsize
+                        }  
+}
+$FinalResult |Sort-Object -Property Size | select Dir, @{Name="GB"; Expression={[math]::round($_.Size/1GB, 2)}}
+
+}
